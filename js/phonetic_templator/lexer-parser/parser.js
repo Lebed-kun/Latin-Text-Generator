7c4fc6deb650f084f35a62ecbb574117228bab8f
Lebed-kun <lebed-kun@mail.ru>
2019-06-22 21:26:02 +0400

    Adopt lexer-parser for RequireJS

--- js/phonetic_templator/lexer-parser/parser.js
+++ js/phonetic_templator/lexer-parser/parser.js
@@ -1,64 +1,66 @@
-/**
- * 
- * @param Object{
- *  ...,
- *  @param String operatorChar : @param Function operatorFunction
- *  ...
- * } operators 
- * @param Object[] opStack // array of tokens
- * @param Number[] resStack 
- */
-// Clean ops
-var cleanOps = function(operators, opStack, resStack) {
-    return function(token, condition) {
-        let cond = condition(token, opStack);
-       
-        while (cond) {
-            let op = opStack.pop();
-            let argsQueue = [];
-            let arity = operators[op.value].length;
+define(function() {
+    /**
+     * 
+     * @param Object{
+     *  ...,
+     *  @param String operatorChar : @param Function operatorFunction
+     *  ...
+     * } operators 
+     * @param Object[] opStack // array of tokens
+     * @param Number[] resStack 
+     */
+    // Clean ops
+    var cleanOps = function(operators, opStack, resStack) {
+        return function(token, condition) {
+            let cond = condition(token, opStack);
+        
+            while (cond) {
+                let op = opStack.pop();
+                let argsQueue = [];
+                let arity = operators[op.value].length;
 
-            for (let j = 1; j <= arity; j++) {
-              argsQueue.push(resStack.pop());
+                for (let j = 1; j <= arity; j++) {
+                argsQueue.push(resStack.pop());
+                }
+                
+                let result = operators[op.value].apply(null, argsQueue.reverse());
+                resStack.push(result);
+                
+                cond = condition(token, opStack);
             }
-            
-            let result = operators[op.value].apply(null, argsQueue.reverse());
-            resStack.push(result);
-            
-            cond = condition(token, opStack);
         }
     }
-}
 
-/**
- * 
- * @param Object{
- *  ...,
- *  @param String tokenType : @param Function(@param Object operators, @param Object[] opStack, @param Number[] resStack) action
- *  ...
- * } rules 
- * @param Object[] operators 
- */
+    /**
+     * 
+     * @param Object{
+     *  ...,
+     *  @param String tokenType : @param Function(@param Object operators, @param Object[] opStack, @param Number[] resStack) action
+     *  ...
+     * } rules 
+     * @param Object[] operators 
+     */
 
-var parser = function(rules, operators) {
-    return function(tokens) {
-        var opStack = [];
-        var resStack = [];
-        
-        for (let i = 0; i < tokens.length; i++) {
-            let action = rules[tokens[i].type](operators, opStack, resStack);
-            action(tokens[i], cleanOps);
-        }
-        
-        cleanOps(operators, opStack, resStack)(
-            null,
-            (token, opStack) => {
-                return opStack.length > 0;
+    var parser = function(rules, operators) {
+        return function(tokens) {
+            var opStack = [];
+            var resStack = [];
+            
+            for (let i = 0; i < tokens.length; i++) {
+                let action = rules[tokens[i].type](operators, opStack, resStack);
+                action(tokens[i], cleanOps);
             }
-        );
-        
-        return resStack[0];
+            
+            cleanOps(operators, opStack, resStack)(
+                null,
+                (token, opStack) => {
+                    return opStack.length > 0;
+                }
+            );
+            
+            return resStack[0];
+        }
     }
-}
 
-module.exports = parser;
+    return parser;
+});
